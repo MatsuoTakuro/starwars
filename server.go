f34e3f272739fb0ce468e7ec4b954b522c44fefc
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-03-27 23:41:24 +0900

    Update server starter

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/MatsuoTakuro/starwars/graph/generated"
@@ -18,11 +21,18 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(starwars.NewResolver()))
+	srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+		rc := graphql.GetFieldContext(ctx)
+		fmt.Println("Entered", rc.Object, rc.Field.Name)
+		res, err = next(ctx)
+		fmt.Println("Left", rc.Object, rc.Field.Name, "=>", res, err)
+		return res, err
+	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/", playground.Handler("Lesson Starwars", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Printf("connect to http://localhost:%s/ for Lesson Starwars", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
