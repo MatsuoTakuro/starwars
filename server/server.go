1da0d9a48b1cf09cb2c7318dc193109befd64748
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-03-30 21:27:43 +0900

    Change debugging prints a little

--- server/server.go
+++ server/server.go
@@ -23,13 +23,12 @@ func main() {
 		port = defaultPort
 	}
 
-	// TODO: #1 Implement resolvers
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(resolver.NewResolver()))
 	srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
 		rc := graphql.GetFieldContext(ctx)
-		fmt.Println("Entered", rc.Object, rc.Field.Name)
+		fmt.Println("\nEntered", rc.Object, rc.Field.Name)
 		res, err = next(ctx)
-		fmt.Println("Left", rc.Object, rc.Field.Name, "=>", res, err)
+		fmt.Println("Output", rc.Object, rc.Field.Name, "=>", res, err)
 		return res, err
 	})
 
